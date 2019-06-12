300b1ad1f97bb1dbd78c735e36a49fbf40154d9e
giuliobosco <giuliobva@gmail.com>
2019-06-12 15:09:42 +0200

    expo init

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Hello World</Text>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
